f859e708950bd16aa187059af33c4f906db591cf
filippemt <filippe.pp@gmail.com>
2021-04-26 21:54:27 -0300

    Alter calculater to Person

---
+++ RestAspNet/RestAspNet/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestAspNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculaterController : ControllerBase
+    {
+
+        private readonly ILogger<CalculaterController> _logger;
+
+        public CalculaterController(ILogger<CalculaterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+      
+            return BadRequest("Invalid Input");
+        }
+    }
+}
