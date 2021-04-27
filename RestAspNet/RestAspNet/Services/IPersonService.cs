86d14a5ef3ccec06fd7be7715269f0f514563ecb
filippemt <filippe.pp@gmail.com>
2021-04-26 22:00:12 -0300

    create Person Interface

---
+++ RestAspNet/RestAspNet/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestAspNet.Model;
+using System.Collections.Generic;
+
+namespace RestAspNet.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        void Delete(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+    }
+}
